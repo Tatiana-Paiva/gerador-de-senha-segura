dbac4251e5788a2c8c4068af46b7327e5c695c8c
Tatiana-Paiva <boutiquepinkmood@gmail.com>
2022-08-27 15:12:54 -0300

    Iniciando JavaScript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,25 @@
-console.log("Testando");
+// Seleção de Elementos
+
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Funções
+const getLetterLowerCase = () => {
+  return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+};
+
+const getLetterUperCase = () => {
+    return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+  };
+
+  const getNumber = () => {
+    return Math.floor( Math.random() * 10).toString();
+  }
+
+  console.log(getNumber());
+
+// Eventos
+
+generatePasswordButton.addEventListener("click", () => {
+    console.log("Teste");
+})
