ae538a76e01927e3fa7d299f1bb1700fd447c386
Tatiana-Paiva <boutiquepinkmood@gmail.com>
2022-08-29 18:50:54 -0300

    Refazendo código JS pois estava com erro

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,51 +4,28 @@ const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
+
 const getLetterLowerCase = () => {
-  return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
-};
+     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
-const getLetterUperCase = () => {
+const getLetterUperCase = () =>  {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  };
+}
 
-  const getNumber = () => {
-    return Math.floor( Math.random() * 10).toString();
-  }
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
 
-  const getSymbol = () => {
-    const symbols = "() {} [] = <> / , . ! @ & # $ % - _ +";
-    return symbols[Math.floor(Math.random() * symbols.length)];
-  }
+const getSymbol = () => {
+const symbols = "() {} [] ! _ - + = , . _ $ & # @ ' %  < > /";
+return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
- const generatePassword = (getLetterLowerCase, getLetterUperCase, getNumber, getSymbol) => {
-  let password = "";
-
-  const passwordLength = 10;
-
-  const generators = [
-    getLetterLowerCase,
-    getLetterUperCase,
-    getNumber,
-    getSymbol
-  ]
-  for(i = 0; i < passwordLength; i = i + generators.length) {
-    generators.forEach(() =>{
-      const randomValue = generators[Math.floor(Math.random() * generators.length)]()
-
-      console.log(randomValue);
-    } )
-  }
-
- }
+console.log(getSymbol());
 
 // Eventos
 
 generatePasswordButton.addEventListener("click", () => {
-   generatePassword(
-     getLetterLowerCase,
-     getLetterUperCase,
-     getNumber,
-     getSymbol
-    );
+    console.log("testando evento");
 })
